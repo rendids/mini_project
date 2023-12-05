4b3f50172a3f9f0d0a813a8a3b56e36fe49c86b9
auliyaww <annisasuci051@gmail.com>
2023-12-05 09:39:10 +0700

    perbaikitampilanmemesan

--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -35,39 +35,9 @@
                                 </ul>
                                 <br>
                                 <h2>Harga : {{ $sedia->harga }}</h2>
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <a href="{{ route('memesan', ['id' => $sedia->id]) }}" class="btn btn-primary" >
                                     pesan
-                                  </button>
-
-                                  <!-- Modal -->
-                                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                      <div class="modal-content">
-                                        <div class="modal-header">
-                                          <h5 class="modal-title" id="exampleModalLabel">Buat Pesanan</h5>
-                                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                          <label for="name">Nama</label>
-                                          <input type="text" name="name" class="form-control" placeholder="masukan nama anda" value="{{ old('value') }}" id="">
-                                          <label for="name">Nama Penyedia Layanan</label>
-                                          <input type="text" name="name" class="form-control" readonly value="{{ $sedia->user->name }}" id="">
-                                          <label for="name">No hp Penyedia</label>
-                                          <input type="text" name="name" class="form-control" readonly value="{{ $sedia->telp }}" id="">
-                                          <label for="name">Layanan</label>
-                                          <input type="text" name="name" class="form-control" readonly value="{{ $sedia->layanan }}" id="">
-                                          <label for="name">Alamat</label>
-                                          <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="masukan alamat anda"></textarea>
-                                          <label for="">hari</label>
-                                          <input type="datetime-local" class="form-control" name="" id="">
-                                        </div>
-                                        <div class="modal-footer">
-                                          <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Batal</button>
-                                          <button type="submit" class="btn btn-primary">ya</button>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
+                                </a>
                             </div>
                         </div>
                     </div>
--- resources/views/user/memesan.blade.php
+++ resources/views/user/memesan.blade.php
@@ -1,38 +1,53 @@
 @extends('layoutsuser.appprofile')
 @section('profile')
+<style>
+    .card {
+      box-shadow: 0 20px 20px rgba(2, 0, 0, 0.1); /* atur warna dan intensitas sesuai kebutuhan */
+    }
+  </style>
     <div class="tab-pane fade show active" id="pills-grid" role="tabpanel" aria-labelledby="pills-grid-tab">
         <div class="row">
             <div class="col-xl-3 col-xxl-4 col-sm-6 ">
             </div>
-            <div class="card mb-3 mt-5" style="width:1500px; height:auto">
+            <div class="card mt-5" style="height:150px; background-color: rgba(4, 0, 255, 0.685); color: white;">
+                <div class="card-body">
+                  <legend>Pesan Layanan Yang Anda Butuhkan Sekarang</legend>
+                  <p>
+                    Dapatkan pengalaman terbaik dari jasa layanan unggulan kami.
+                    Lihat detail lengkap pelayanan sebelum membuat keputusan.
+                    Pastikan data yang Anda berikan akurat agar kami dapat memproses pesanan dengan lancar.
+                    Pastikan alamat Anda benar untuk memastikan layanan yang tepat waktu.
+                  </p>
+                </div>
+              </div>
+              <div class="card mb-3" style="width:1500px; height:auto">
                 <div class="row g-0">
                   <div class="col-md-12">
                     <div class="card-body">
-                        <div class="row ">
+                      <div class="row">
                         <div class="col-6">
-                        <label for="name">Nama</label>
-                        <input type="text" name="name" class="form-control" placeholder="masukan nama anda" value="{{ old('value') }}" id="">
-                        <label for="name">Nama Penyedia Layanan</label>
-                        <input type="text" name="name" class="form-control" readonly value="{{ $sedia->user->name }}" id="">
-                        <label for="name">No hp Penyedia</label>
-                        <input type="text" name="name" class="form-control" readonly value="{{ $sedia->telp }}" id="">
+                          <label for="name" class="fs-4 fw-bold">Nama</label>
+                          <input type="text" name="name" class="form-control" placeholder="masukkan nama anda" value="{{ old('value') }}" id="">
+                          <label for="name" class="fs-4 fw-bold">Nama Penyedia Layanan</label>
+                          <input type="text" name="name" class="form-control" readonly value="{{ $sedia->user->name }}" id="">
+                          <label for="name" class="fs-4 fw-bold">No hp Penyedia</label>
+                          <input type="text" name="name" class="form-control" readonly value="{{ $sedia->telp }}" id="">
                         </div>
                         <div class="col-6">
-                        <label for="name">Layanan</label>
-                        <input type="text" name="name" class="form-control" readonly value="{{ $sedia->layanan }}" id="">
-                        <label for="name">Alamat</label>
-                        <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="masukan alamat anda"></textarea>
-                        <label for="">hari</label>
-                        <input type="datetime-local" class="form-control" name="" id="">
-                    </div>
+                          <label for="name" class="fs-4 fw-bold">Layanan</label>
+                          <input type="text" name="name" class="form-control" readonly value="{{ $sedia->layanan }}" id="">
+                          <label for="name" class="fs-4 fw-bold">Alamat</label>
+                          <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="masukkan alamat anda"></textarea>
+                          <label for="" class="fs-4 fw-bold">hari</label>
+                          <input type="datetime-local" class="form-control" name="" id="">
                         </div>
                       </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Batal</button>
-                        <button type="submit" class="btn btn-primary">ya</button>
+                    </div>
+                    <div class="modal-footer">
+                      <a href="{{ route('detail', ['id' => $sedia->id]) }}" type="button" class="btn btn-outline-danger">Batal</a>
+                      <button type="submit" class="btn btn-primary">Lanjutkan</button>
                     </div>
                   </div>
                 </div>
               </div>
-    </div>
 @endsection
