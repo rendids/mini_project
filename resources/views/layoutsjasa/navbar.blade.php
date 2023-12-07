ae96fa4b785bc77ca828d2e8b1351d17b903669e
auliyaww <annisasuci051@gmail.com>
2023-12-07 16:02:08 +0700

    icon notifikasi

--- resources/views/layoutsjasa/navbar.blade.php
+++ resources/views/layoutsjasa/navbar.blade.php
@@ -31,11 +31,12 @@
 						<div class="nav-item d-flex align-items-center">
 						</div>
 					</div>
-
 					<ul class="navbar-nav header-right ">
 
 						<li class="nav-item d-flex align-items-center">
-
+                            <div class="dropdown header-profile2" style="margin-top: 100%;">
+                            <i class="bi bi-bell" style="font-size: 24px;"></i>
+                            </div>
 						</li>
 						<li>
 
@@ -59,8 +60,10 @@
 									</a>
 									<a href="{{ route('logout') }}" class="dropdown-item ai-icon ms-1">
 										<svg  xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+
 										<span class="ms-1">Logout </span>
 									</a>
+                                    <i class="fa-solid fa-bell"></i>
 								</div>
 							</div>
 						</li>
